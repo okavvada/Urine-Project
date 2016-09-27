02b79722fd4571f177bb3d01042427352d6d9173
Olga <okavvada@gmail.com>
2016-09-27 15:14:06 -0700

    deleted duplicate facility count

--- Run_LCA_model.py
+++ Run_LCA_model.py
@@ -16,6 +16,7 @@ def Run_LCA_model(path, n_regen, n_collection, schedule, logistics):
 
 	Total_Energy = pd.DataFrame()
 	Total_GHG = pd.DataFrame()
+	Total_COST = pd.DataFrame()
 
 	for index, row in distance_regeneration.iterrows():
 	    number_of_people_per_facility= row['num_people']
@@ -24,7 +25,7 @@ def Run_LCA_model(path, n_regen, n_collection, schedule, logistics):
 	    ENERGY, GHG, COST= LCA_urine_model(number_of_people_per_facility, distance_regen, truck_num)
 	    Total_Energy=Total_Energy.append(ENERGY)
 	    Total_GHG=Total_GHG.append(GHG)
-	    Total_COST=Total_GHG.append(COST)
+	    Total_COST=Total_COST.append(COST)
 
 	Total_Energy_regen = Total_Energy.sum()
 	Total_Energy_regen=pd.DataFrame(Total_Energy_regen).T
@@ -41,7 +42,6 @@ def Run_LCA_model(path, n_regen, n_collection, schedule, logistics):
 	Total_Energy_total['Logistics_collect'] = Total_Energy_collect
 	Total_GHG_total['Logistics_collect'] = Total_GHG_collect
 	Total_COST_total['Logistics_collect'] = Total_COST_collect
-	Total_COST_total['Facilities_space_new'] = facility_manufacturing_cost*n_regen
 	urine_total = urine_production_scaled/1000
 	Total_Energy_m3=Total_Energy_total/(urine_total*365*Total_people)
 	Total_Energy_m3['n_facilities'] = n_regen
